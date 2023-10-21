90cbcf598fa92cafe192dbb846816970bce27a72
Dalton Woodside-Moffatt <dwoodsidemoffatt@gmail.com>
2023-10-20 20:12:28 -0700

    Added tests for x_train property

--- tests/unit/interface/test_x_train.py
+++ tests/unit/interface/test_x_train.py
@@ -1,3 +1,6 @@
+"""
+Tests x_train property of MLInterface Class
+"""
 import numpy as np
 from pytest_check import check
 from src.MLInterface import MLInterface
@@ -17,6 +20,21 @@ class TestXTrain:
         :return:
         """
         check.is_instance(self.ml_interface.x_train, np.ndarray)
+
+    def test_x_train_setter(self):
+        """
+        Tests that x_train can be written to, and only accepts values of np.ndarray type
+
+        :return:
+        """
+        valid_data = np.array([400.0, 300.00, 500.00])
+
+        self.ml_interface.x_train = valid_data
+        check.is_(self.ml_interface.x_train, valid_data)
+
+        invalid_data = 1
+        with check.raises(TypeError):
+            self.ml_interface.x_train = invalid_data
 
     def teardown_method(self, method):
         del self.ml_interface
